ec991e03e967fa17e5a3d987a7ae655cf7f6899a
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-05-01 23:35:31 +0530

    ADDED : Vehicle Parked

---
+++ src/main/java/com/parking/ParkingLot.java
@@ -0,0 +1,10 @@
+package com.parking;
+
+public class ParkingLot {
+    private Object vehicle;
+
+    public boolean parkVehicle(Object vehicle) {
+        this.vehicle = vehicle;
+        return true;
+    }
+}
---
+++ src/test/java/com/parkingtest/ParkingTest.java
@@ -0,0 +1,23 @@
+package com.parkingtest;
+
+import com.parking.ParkingLot;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ParkingTest {
+    static Object vehicle = null;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        vehicle = new Object();
+    }
+
+    @Test
+    public void givenVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLot parkingLot = new ParkingLot();
+        boolean isParked = parkingLot.parkVehicle(vehicle);
+        Assert.assertTrue(isParked);
+    }
+
+}
